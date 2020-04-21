de7c57dd02d8385a657bd0b40bc3734a0df40193
josephchilin <joseph@josephchilin.com>
2020-04-21 14:35:28 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+    attr_accessor :name 
+    attr_accessor :breed
+    
+    def initialize(name="dog name", breed="dog breed")
+        @name = name
+        @breed = breed
+    end
+
+    # def breed=(breed)
+    #     @breed = breed
+    # end
+
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # def name
+    #     @name
+    # end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+
+    attr_accessor :name
+    attr_accessor :job
+    def initialize(name="person name", job="job name")
+        @name = name
+        @job = job
+    end
+
+    # def name
+
+
+    # end
+
+end
